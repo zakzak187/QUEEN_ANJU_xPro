9061c3f1e890052a45f091ac327618e447eee002
BOT_RASHMIKA <janith60022006@gmail.com>
2025-01-29 14:25:40 +0530

    | 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗫ᴾᴿᴼ

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session id here",
+SESSION_ID: process.env.SESSION_ID || "Your session id here",
 PORT: process.env.PORT || "8000"
 };
